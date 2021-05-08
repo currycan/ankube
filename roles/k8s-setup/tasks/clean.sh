b2bd2a888b4b3da3557aec0500e160388772dc4f
zhangzhitao <zhangzhitao@lakala.com>
2021-05-08 16:42:29 +0800

    fix: 一些基础优化

--- roles/k8s-setup/tasks/clean.sh
+++ roles/k8s-setup/tasks/clean.sh
@@ -1,8 +1,9 @@
 rm -rf /etc/kubernetes/manifests/
 systemctl stop kubelet.service
 docker ps -aq | xargs docker rm -f
+find /var/lib/kubelet | xargs -n 1 findmnt -n -t tmpfs -o TARGET -T | uniq | xargs -r umount -v
 rm -rf /var/lib/kubelet/
-rm -rf /var/lib/kubelet/cpu_manager_state
+rm -rf /var/lib/kubelet/*_manager_state
 kubeadm init --config /etc/kubernetes/kubeadm-config.yaml
 
 # 升级
@@ -14,5 +15,5 @@ systemctl stop kubelet.service
 rm -rf /etc/kubernetes/manifests/
 docker ps -aq | xargs docker rm -f
 rm -rf /var/lib/kubelet/
-rm -rf /var/lib/kubelet/cpu_manager_state
+rm -rf /var/lib/kubelet/*_manager_state
 rm -f /etc/kubernetes/kubelet.conf
